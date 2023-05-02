41ebd6c3a8fb703d42efa2f2928b6af1290c745f
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-05-02 06:48:08 -0700

    Added Banner aswell as adfly ads

--- content.js
+++ content.js
@@ -14,5 +14,23 @@ const code = `
   <script src="https://cdn.adf.ly/js/entry.js"></script>
 `;
 
+const banner = `
+  <!-- Start of adf.ly banner code -->
+  <div style="position:relative;width: 728px; height:90px; text-align: center; font-family: verdana; font-size: 10px;">
+    <div style="position:absolute; top:0; left:0;">
+      <a href="https://join-adf.ly/24692891">
+        <img border="0" src="https://cdn.adf.ly/images/banners/adfly.728x90.2.gif" width="728" height="90" title="AdF.ly - shorten links and earn money!" />
+      </a>
+    </div>
+    <div style="height:90px;"></div>
+  </div>
+  <!-- End of adf.ly banner code -->
+`;
+
+const body = document.body || document.getElementsByTagName("body")[0];
+body.insertAdjacentHTML("afterbegin", banner);
+console.log("Adf.ly banner inserted into the body element at the top-middle!");
+
 const head = document.head || document.getElementsByTagName("head")[0];
 head.insertAdjacentHTML("beforeend", code);
+console.log("Adf.ly script inserted into the head element!");
